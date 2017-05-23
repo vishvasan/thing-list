fc054e850f13663bc4a74665427a7a60ab2fca69
Vishal Vasan <vishalvasan@vishals-mbp.coldspring.marian.edu>
2017-05-23 13:32:54 -0400

    Make things pretty

--- src/ThingList.js
+++ src/ThingList.js
@@ -3,14 +3,15 @@ import './ThingList.css'
 import Thing from './Thing'
 
 const ThingList = (props) => {
-    return (
-        <ul className="ThingList">
-           { 
-               Object.keys(props.things)
-               .map(key => <Thing thing={} />) 
-            } 
-        </ul>
-    )
+  return (
+    <ul className="ThingList">
+      {
+        Object
+          .keys(props.things)
+          .map(thingId => <Thing thing={props.things[thingId]} key={thingId} />)
+      }
+    </ul>
+  )
 }
 
 export default ThingList
